1f3fd31332f38f53b06ded60c0d14a40f65d3fc1
jp <acarvalho.jp@gmail.com>
2025-04-15 21:52:18 +0100

    feat: user post

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -11,4 +11,65 @@ const getUsers = async (req, res) => {
     }
 };
 
-module.exports = { getUsers };
+const addUser = async (req, res) => {
+    const {
+        nomeUtilizador,
+        dataNascimento,
+        pontosUtilizador,
+        comprovativoResidencia,
+        emailUtilizador,
+        password,
+        VizinhançaidVizinhança,
+        EnderecoidEndereco,
+        estadoUtilizadoridEstadoUtilizador,
+        tipoUtilizadoridTipoUtilizador
+    } = req.body;
+
+    try {
+        const pool = await poolPromise;
+        await pool.request()
+            .input('nomeUtilizador', nomeUtilizador)
+            .input('dataNascimento', dataNascimento)
+            .input('pontosUtilizador', pontosUtilizador)
+            .input('comprovativoResidencia', comprovativoResidencia)
+            .input('emailUtilizador', emailUtilizador)
+            .input('password', password)
+            .input('idVizinhanca', VizinhançaidVizinhança)
+            .input('idEndereco', EnderecoidEndereco)
+            .input('idEstadoUtilizador', estadoUtilizadoridEstadoUtilizador)
+            .input('idTipoUtilizador', tipoUtilizadoridTipoUtilizador)
+            .query(`
+                INSERT INTO [dbo].[Utilizador] (
+                    nomeUtilizador,
+                    dataNascimento,
+                    pontosUtilizador,
+                    comprovativoResidencia,
+                    emailUtilizador,
+                    password,
+                    VizinhançaidVizinhança,
+                    EnderecoidEndereco,
+                    estadoUtilizadoridEstadoUtilizador,
+                    tipoUtilizadoridTipoUtilizador
+                )
+                VALUES (
+                    @nomeUtilizador,
+                    @dataNascimento,
+                    @pontosUtilizador,
+                    @comprovativoResidencia,
+                    @emailUtilizador,
+                    @password,
+                    @idVizinhanca,
+                    @idEndereco,
+                    @idEstadoUtilizador,
+                    @idTipoUtilizador
+                )
+            `);
+
+        res.status(201).json({ message: "User added successfully" });
+    } catch (error) {
+        console.error("Error adding user in database:", error);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+module.exports = { getUsers, addUser };
