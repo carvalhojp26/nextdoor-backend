018feab22d3ec510a3bf80f013446454ac30706a
jp <acarvalho.jp@gmail.com>
2025-04-24 02:50:55 +0100

    feat: authenticateToken middleware

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -9,6 +9,7 @@ const {
   loginUserController
 } = require("../controllers/userController");
 const sqlInjectionGuard = require("../middlewares/sqlInjectionGuard");
+const authenticateToken = require("../middlewares/authenticateToken");
 
 router.get("/", sqlInjectionGuard, listUsers);
 //router.post("/", sqlInjectionGuard, addUser);
@@ -16,5 +17,9 @@ router.post("/register", sqlInjectionGuard, registerUserController)
 router.post("/login", sqlInjectionGuard, loginUserController)
 router.put("/:userId", sqlInjectionGuard, editUser);
 router.delete("/:userId", sqlInjectionGuard, deleteUserController);
+
+router.get("/protected-route", authenticateToken, (req, res) => {
+  res.json({ message: "You are authenticated!" });
+})
 
 module.exports = router;
